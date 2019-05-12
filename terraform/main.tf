8f9ad2f3608c42c553fd20fac6db945146b047c6
Andrew Murray <andrew.murray@arnoldclark.com>
2019-05-12 08:38:36 +0100

    Added topic, queue, auth rules to service bus config

--- terraform/main.tf
+++ terraform/main.tf
@@ -3,17 +3,47 @@ provider azurerm {}
 provider tls {}
 
 resource "azurerm_resource_group" "rg" {
-  name     = "amu-service-bus-tryout-rg"
-  location = "uksouth"
+  name     = "${var.prefix}-rg"
+  location = "${var.location}"
 
   tags = {
     environment = "Development"
-  }
+  }}
+
+resource "azurerm_servicebus_namespace" "sbnamespace" {
+  name                = "${var.prefix}-sbnamespace"
+  location            = "${azurerm_resource_group.rg.location}"
+  resource_group_name = "${azurerm_resource_group.rg.name}"
+  sku                 = "Standard"
 }
 
-resource "azurerm_servicebus_namespace" "amu_service_bus_tryout" {
-  name                = "amu-service-bus-tryout"
+resource "azurerm_servicebus_namespace_authorization_rule" "authrule" {
+  name                = "${var.prefix}-sbnauth"
+  namespace_name      = "${azurerm_servicebus_namespace.sbnamespace.name}"
   resource_group_name = "${azurerm_resource_group.rg.name}"
-  location            = "${azurerm_resource_group.rg.location}"
-  sku                 = "Standard"
+  send                = true
+  listen              = true
+  manage              = true
+}
+
+resource "azurerm_servicebus_topic" "topic" {
+  name                = "${var.prefix}-sbtopic"
+  resource_group_name = "${azurerm_resource_group.rg.name}"
+  namespace_name      = "${azurerm_servicebus_namespace.sbnamespace.name}"
+  enable_partitioning = true
+}
+
+resource "azurerm_servicebus_subscription" "subscription" {
+  name                = "${var.prefix}-sbsubscription"
+  resource_group_name = "${azurerm_resource_group.rg.name}"
+  namespace_name      = "${azurerm_servicebus_namespace.sbnamespace.name}"
+  topic_name          = "${azurerm_servicebus_topic.topic.name}"
+  max_delivery_count  = 1
+}
+
+resource "azurerm_servicebus_queue" "queue" {
+  name                = "${var.prefix}-sbqueue"
+  resource_group_name = "${azurerm_resource_group.rg.name}"
+  namespace_name      = "${azurerm_servicebus_namespace.sbnamespace.name}"
+  enable_partitioning = true
 }
